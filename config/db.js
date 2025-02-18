32565b6cb329de2a775590f23104a8b0dffefeb4
dareyno4 <destinyreynolds2002@gmail.com>
2025-02-18 15:09:09 -0700

    config files, db - day1

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose'); // Import mongoose for MongoDB object modeling
+
+const connectDB = async() => { // Function to connect to MongoDB
+    try {
+        await mongoose.connect(process.env.MANGO_URI, { // Connect to MongoDB using the URI from environment variables
+            useNewUrlParser: true, // Use the new URL parser
+            useUnifiedTopology: true, // Use the new unified topology engine
+        });
+        console.log('MongoDB connected...');
+        }
+    catch (err) {
+        console.error("MangoDB connection error:", err.message); // Log any connection errors
+        process.exit(1); // Exit the process with failure
+    }
+};
+module.exports = connectDB; // Export the connectDB function for use in other files
